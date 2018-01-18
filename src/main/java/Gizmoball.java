c57d3308324ad4d25e39fe1f09760501d5299fa9
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2018-01-18 03:55:34 +0000

    Set up the scene files

--- src/main/java/Gizmoball.java
+++ src/main/java/Gizmoball.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Gizmoball extends Application {
 
@@ -9,6 +14,21 @@ public class Gizmoball extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("Gizmoball.fxml"));
+        try {
+            Scene scene = new Scene(loader.load());
+            scene.getStylesheets().add(getClass().getResource("Gizmoball.css").toString());
+            primaryStage.setScene(scene);
+        } catch (IOException e) {
+            System.out.println("Error loading fxml file");
+            Platform.exit();
+        }
 
+        Controller controller = loader.getController();
+
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Gizmoball");
+        primaryStage.show();
     }
 }
